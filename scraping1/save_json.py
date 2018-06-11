73ba928af3d2fa4e843addf09675044ce7a0c757
rabelais88 <sungryeolp@gmail.com>
2018-06-12 05:55:01 +0900

    180612

--- scraping1/save_json.py
+++ scraping1/save_json.py
@@ -3,8 +3,7 @@ import random
 
 rndgen = lambda min, max: str(random.randint(min, max))
 cities = ['incheon', 'seoul', 'busan', 'bucheon', 'jeonnam']
-rowsdata = list(map(lambda x: {'rank': x, 'city': cities[x], 'population': rndgen(100,50000)}, range(5)))
-
+rowsdata = list(map(lambda x: {'rank': x, 'city': cities[x], 'population': rndgen(100, 50000)}, range(5)))
 with open('top_cities.json', 'w') as f:
   json.dump(rowsdata, f)
 
